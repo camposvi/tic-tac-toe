7ab0a99c6c23cb823dc0b5576b6cbb634ca59dde
Vinicius Campos <viniciuscampos4@gmail.com>
2025-03-17 23:53:54 -0300

    feat:create logic for handle active player and handle selected square

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
+import { useState } from 'react';
 import Header from './components/Header/Header';
 import Player from './components/Player/Player';
 import GameBoard from './components/GameBoard/GameBoard';
 function App() {
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handleSelectSquare() {
+    setActivePlayer((currentPlayer) => (currentPlayer === 'X' ? 'O' : 'X'));
+  }
   return (
     <>
       <Header />
       <main>
         <div id="game-container">
-          <ol id="players">
-            <Player initialName="Player1" symbol="X" />
-            <Player initialName="Player2" symbol="O" />
+          <ol id="players" className="highlight-player">
+            <Player
+              initialName="Player1"
+              symbol="X"
+              isActive={activePlayer === 'X'}
+            />
+            <Player
+              initialName="Player2"
+              symbol="O"
+              isActive={activePlayer === 'O'}
+            />
           </ol>
-          <GameBoard />
+          <GameBoard
+            onSelectSquare={handleSelectSquare}
+            activePlayerSymbol={activePlayer}
+          />
         </div>
         LOG
       </main>
