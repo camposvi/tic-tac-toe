be2cda0699288905bcc38c9b05de645d301c9470
Vinicius Campos <viniciuscampos4@gmail.com>
2025-03-19 13:30:43 -0300

    feat: include Log.jsx logic to display the player turn, passing gameTurns as a prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import { useState } from 'react';
-import Header from './components/Header/Header';
-import Player from './components/Player/Player';
-import GameBoard from './components/GameBoard/GameBoard';
+import Header from './components/Header/Header.jsx';
+import Player from './components/Player/Player.jsx';
+import GameBoard from './components/GameBoard/GameBoard.jsx';
+import Log from './components/Log/Log.jsx';
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState('X');
 
-  function handleSelectSquare() {
+  function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((currentPlayer) => (currentPlayer === 'X' ? 'O' : 'X'));
+    setGameTurns((prevTurns) => {
+      let currentPlayer = 'X';
+      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
+        currentPlayer = 'O';
+      }
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+      return updatedTurns;
+    });
   }
   return (
     <>
@@ -25,12 +38,9 @@ function App() {
               isActive={activePlayer === 'O'}
             />
           </ol>
-          <GameBoard
-            onSelectSquare={handleSelectSquare}
-            activePlayerSymbol={activePlayer}
-          />
+          <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
         </div>
-        LOG
+        <Log turns={gameTurns} />
       </main>
     </>
   );
