631a1478e813ef41fdddfec1d2e173f6985b0dea
Vinicius Campos <viniciuscampos4@gmail.com>
2025-03-19 23:20:57 -0300

    refactor: remove unnecessary state management

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,22 @@ import Header from './components/Header/Header.jsx';
 import Player from './components/Player/Player.jsx';
 import GameBoard from './components/GameBoard/GameBoard.jsx';
 import Log from './components/Log/Log.jsx';
+const deriveActivePlayer = (gameTurns) => {
+  let currentPlayer = 'X';
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+  return currentPlayer;
+};
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
 
+  //const [activePlayer, setActivePlayer] = useState('X');
+  const activePlayer = deriveActivePlayer(gameTurns);
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((currentPlayer) => (currentPlayer === 'X' ? 'O' : 'X'));
+    //setActivePlayer((currentPlayer) => (currentPlayer === 'X' ? 'O' : 'X'));
     setGameTurns((prevTurns) => {
-      let currentPlayer = 'X';
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-        currentPlayer = 'O';
-      }
+      const currentPlayer = deriveActivePlayer(prevTurns);
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
         ...prevTurns,
